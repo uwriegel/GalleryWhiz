a694105d9cf05f6fd8a39ef81c7db5cf7c699645
Uwe Riegel <uwriegel@gmail.com>
2018-01-29 18:37:50 +0100

    ColumnsControl, Scrollbar

---
+++ GalleryWhiz/WebApp/scripts/columnscontrol.js
@@ -0,0 +1,211 @@
+﻿const createColumnsControl = (function () {
+    /**
+     * 
+     * @param {string} viewId
+     */
+    const createColumnsControl = (viewId) => (columnId) => (columns) => {
+        const id = `${viewId}-${columnId}`
+        const itemTemplate = document.createElement("tr")
+        itemTemplate.style.userSelect = "none"
+        const td = document.createElement("td")
+        td.classList.add("it-name")
+        const nameDiv = document.createElement("div")
+        nameDiv.classList.add("it-iconName")
+        const img = document.createElement("img")
+        img.classList.add("it-image")
+        img.src = 'assets/images/fault.png'
+        img.alt = ""
+        img.onerror = () => img.src = 'assets/images/fault.png'
+        const span = document.createElement("span")
+        span.classList.add("it-nameValue")
+        nameDiv.appendChild(img)
+        nameDiv.appendChild(span)
+        td.appendChild(nameDiv)
+        itemTemplate.appendChild(td)
+        itemTemplate.tabIndex = 0
+
+        for (let i = 1; i < columns.length; i++) {
+            const td = document.createElement("td")
+            itemTemplate.appendChild(td)
+        }
+
+        /**
+         * Initialisierung des ColumnControls
+         * @param {HTMLElement} tableViewToSet Das HTMLElement der zugehörigen TableView
+         * @param {any} viewToSet
+         */
+        function initialize(tableViewToSet, viewToSet) {
+            tableView = tableViewToSet
+            //view = viewToSet
+            const ths = tableView.getElementsByTagName("th")
+
+            const json = localStorage[id]
+            if (json) {
+                const columnWidth = JSON.parse(json)
+                setWidths(columnWidth)
+            }
+            else
+                setWidths()
+
+            let grippingReady = false;
+            Array.from(ths).forEach((th, columnIndex) => {
+                th.onmousedown = evt => {
+                    const column = evt.target
+                    if (!grippingReady) {
+                            // TODO:                    
+                            //const ascending = column.classList.contains("sortAscending")
+                            // if (view.getPresenter().sort(columnIndex, !ascending)) {
+                            //     for (let i = 0; i < ths.length; i++) {
+                            //         ths[i].classList.remove("sortAscending")
+                            //         ths[i].classList.remove("sortDescending")
+                            //     }
+
+                            //     column.classList.add(ascending ? "sortDescending" : "sortAscending")
+                            // }
+                        }
+                        else
+                        beginColumnDragging(evt.pageX, column)
+                }
+            })
+
+            tableView.addEventListener('mousemove', evt => {
+                const th = evt.target
+                if (th.localName == "th" && (th.offsetLeft > 0 || evt.pageX - th.getBoundingClientRect().left > 10)
+                        && (th.offsetLeft + th.offsetWidth < tableView.offsetWidth || evt.pageX - th.getBoundingClientRect().left < 4)
+                        && (th.getBoundingClientRect().left + th.offsetWidth - evt.pageX < 4 || evt.pageX - th.getBoundingClientRect().left < 4))  {
+                                document.body.style.cursor = 'ew-resize'
+                    grippingReady = true
+                    previous = evt.pageX - th.getBoundingClientRect().left < 4
+                            }
+                else {
+                                document.body.style.cursor = 'default'
+                    grippingReady = false
+                }
+            });
+        }
+
+        /**
+         * 
+         * @param {(item: string)=>void} eachColumnAction
+         */
+        function initializeEachColumn(eachColumnAction) {
+            columns.forEach(eachColumnAction)
+        }
+
+        /**
+         * @returns {HTMLTableRowElement}
+         */
+        function getItemTemplate() {
+            return itemTemplate.cloneNode(true);
+        }
+
+        /**
+         * 
+         * @param {number} startGripPosition
+         * @param {HTMLElement} targetColumn
+         */
+        function beginColumnDragging(startGripPosition, targetColumn) {
+            document.body.style.cursor = 'ew-resize'
+
+            /**
+             * @type {HTMLElement}
+             */
+            let currentHeader
+            if (!previous)
+                currentHeader = targetColumn
+            else
+                currentHeader = targetColumn.previousElementSibling
+            const nextHeader = currentHeader.nextElementSibling
+            const currentLeftWidth = currentHeader.offsetWidth
+            const sumWidth = currentLeftWidth + nextHeader.offsetWidth
+
+            const onmove = evt => {
+                if (evt.which == 0) {
+                    document.body.style.cursor = 'default'
+                    window.removeEventListener('mousemove', onmove)
+                    return
+                }
+                document.body.style.cursor = 'ew-resize'
+
+                var diff = evt.pageX - startGripPosition
+
+                if (currentLeftWidth + diff < 15)
+                    diff = 15 - currentLeftWidth
+                else if (diff > sumWidth - currentLeftWidth - 15)
+                    diff = sumWidth - currentLeftWidth - 15
+
+                const combinedWidth = getCombinedWidth(currentHeader, nextHeader)
+
+                let leftWidth = currentLeftWidth + diff
+                let rightWidth = sumWidth - currentLeftWidth - diff
+                const factor = combinedWidth / sumWidth
+                leftWidth = leftWidth * factor
+                rightWidth = rightWidth * factor
+
+                currentHeader.style.width = leftWidth + '%'
+                nextHeader.style.width = rightWidth + '%'
+                const columnsWidths = getWidths()
+                localStorage[id] = JSON.stringify(columnsWidths)
+
+                evt.preventDefault()
+            }
+
+            window.addEventListener('mousemove', onmove)
+        }
+
+        function getWidths() {
+            let widths = new Array()
+            const ths = tableView.getElementsByTagName("th")
+            Array.from(ths).forEach((th, i) => {
+                                            widths[i] = th.style.width
+                if (!widths[i])
+                    widths[i] = (100 / columns.length) + '%'
+            })
+            return widths
+        }
+
+        /**
+         * @param {string[]} widths
+         */
+        function setWidths(widths) {
+            const ths = tableView.getElementsByTagName("th")
+
+            if (!widths) {
+                const width = 100 / ths.length
+                widths = []
+                for (let i = 0; i < ths.length; i++)
+                    widths.push(width + '%')
+            }
+
+            Array.from(ths).forEach((th, i) => {
+                if (widths)
+                    th.style.width = widths[i]
+            })
+        }
+
+        /**
+         * 
+         * @param {HTMLElement} column
+         * @param {HTMLElement} nextColumn
+         */
+        function getCombinedWidth(column, nextColumn) {
+            const firstWidth = column.style.width ? parseFloat(column.style.width.substr(0, column.style.width.length - 1)) : 100 / columns.length
+            const secondWidth = nextColumn.style.width ? parseFloat(nextColumn.style.width.substr(0, nextColumn.style.width.length - 1)) : 100 / columns.length
+            return firstWidth + secondWidth
+        }
+
+        var previous = false
+        /**
+         * @type {HTMLElement}
+         */
+        var tableView 
+        //var view: View
+
+        return {
+            initialize: initialize,
+            initializeEachColumn: initializeEachColumn,
+            getItemTemplate: getItemTemplate
+        }
+    }
+    return createColumnsControl
+})()
---
+++ GalleryWhiz/WebApp/scripts/scrollbar.js
@@ -0,0 +1,306 @@
+﻿
+const createScrollbar = (function () {
+    /**
+     * Scrollbar der TableView
+     * @param {HTMLElement} parent Das Element, welches mit einer Scrollbar versehen werden soll
+     * @param {(position: number) => void} positionChanged Callback zum Scrollen
+     */
+    function createScrollbar(parent, positionChanged) {
+        /**
+        * Das Div der Scrollbar
+        */
+        const scrollbar = document.createElement("div")
+        scrollbar.classList.add("scrollbar")
+        scrollbar.classList.add("scrollbarHidden")
+
+        const up = document.createElement("div")
+        up.classList.add("scrollbarUp")
+        scrollbar.appendChild(up)
+
+        const upImg = document.createElement("div")
+        upImg.classList.add("scrollbarUpImg")
+        up.appendChild(upImg)
+
+        const down = document.createElement("div")
+        down.classList.add("scrollbarDown")
+        scrollbar.appendChild(down)
+
+        const downImg = document.createElement("div")
+        downImg.classList.add("scrollbarDownImg")
+        down.appendChild(downImg)
+
+        const grip = document.createElement("div")
+        grip.classList.add("scrollbarGrip")
+        scrollbar.appendChild(grip)
+
+        /**
+         * {() => void}
+         */
+        let setFocus
+
+        parent.appendChild(scrollbar)
+
+        up.onmousedown = () => {
+            clearTimeout(timer)
+            clearInterval(interval)
+            mouseUp()
+
+            timer = setTimeout(() => interval = setInterval(mouseUp, 20), 600)
+        }
+
+        down.onmousedown = () => {
+            clearTimeout(timer)
+            clearInterval(interval)
+            mouseDown()
+
+            timer = setTimeout(() => interval = setInterval(mouseDown, 20), 600)
+        }
+
+        scrollbar.onmousedown = evt => {
+            if (!(evt.target).classList.contains("scrollbar"))
+                return
+
+            pageMousePosition = evt.pageY
+            const isPageUp = evt.pageY < grip.offsetTop
+            clearTimeout(timer)
+            clearInterval(interval)
+            if (isPageUp)
+                pageUp()
+            else
+                pageDown()
+
+            timer = setTimeout(() => interval = setInterval((isPageUp ? pageUp : pageDown), 20), 600)
+        }
+
+        grip.onmousedown = evt => {
+            if (evt.which != 1)
+                return
+            gripping = true
+            evt.preventDefault()
+
+            gripTopDelta = grip.offsetTop + scrollbar.offsetTop - evt.pageY
+
+            var gripperMove = evt => {
+                if (!gripping || evt.which != 1) {
+                    window.removeEventListener('mousemove', gripperMove)
+                    return
+                }
+
+                var top = evt.pageY + gripTopDelta - scrollbar.offsetTop
+                if (top < 15)
+                    top = 15
+                else if (top + grip.offsetHeight - 15 > (parentHeight - 32))
+                    top = parentHeight - 32 - grip.offsetHeight + 15
+                grip.style.top = top + 'px'
+
+                var currentPosition = Math.floor((top - 15) / step + 0.5)
+                if (currentPosition != position) {
+                    position = currentPosition
+                    positionChanged(position)
+                }
+            }
+
+            window.addEventListener('mousemove', gripperMove)
+        }
+
+        up.onmouseup = mouseup
+        down.onmouseup = mouseup
+        grip.onmouseup = mouseup
+        scrollbar.onmouseup = mouseup
+
+        scrollbar.onclick = evt => evt.stopPropagation()
+
+        scrollbar.onmouseleave = () => {
+                    clearTimeout(timer)
+            clearInterval(interval)
+        }
+
+        /**
+         * 
+         * @param {()=>void} setFocusToSet
+         */
+        function initialize(setFocusToSet) {
+            setFocus = setFocusToSet
+        }
+
+        /**
+         * 
+         * @param {number} itemsCount
+         * @param {number} tableCapacity
+         * @param {number} newPosition
+         */
+        function itemsChanged(itemsCount, tableCapacity, newPosition) {
+            parentHeight = parent.offsetHeight - offsetTop
+            if (itemsCount)
+                itemsCountAbsolute = itemsCount
+            if (tableCapacity)
+                itemsCountVisible = tableCapacity
+
+            if (!itemsCountAbsolute)
+                return
+
+            if (itemsCountAbsolute <= itemsCountVisible)
+                scrollbar.classList.add("scrollbarHidden")
+            else {
+                    scrollbar.classList.remove("scrollbarHidden")
+                var gripHeight = (parentHeight - 32) * (itemsCountVisible / itemsCountAbsolute)
+                if (gripHeight < 5)
+                    gripHeight = 5
+                steps = itemsCountAbsolute - itemsCountVisible
+                step = (parentHeight - 32 - gripHeight) / steps
+                grip.style.height = gripHeight + 'px'
+                if (position > steps)
+                    position = steps
+            }
+            if (newPosition != undefined)
+                position = newPosition
+            positionGrip()
+        }
+
+        /**
+         * 
+         * @param {number} position
+         */
+        function setPosition(position) {
+            position = position
+            if (position > steps)
+                position = steps
+            if (position < 0)
+                position = 0
+            positionGrip()
+        }
+
+        function mouseup() {
+            clearTimeout(timer)
+            clearInterval(interval)
+            gripping = false
+            setFocus()
+        }
+
+        function mouseUp() {
+            position -= 1
+            if (position < 0) {
+                    position = 0
+                clearTimeout(timer)
+                clearInterval(interval)
+                return
+            }
+
+            positionGrip()
+            positionChanged(position)
+        }
+
+        function mouseDown() {
+            position += 1
+            if (position > steps) {
+                    position = steps
+                clearTimeout(timer)
+                clearInterval(interval)
+                return
+            }
+            positionGrip()
+            positionChanged(position)
+        }
+
+        function pageUp() {
+            if (grip.offsetTop < pageMousePosition) {
+                clearTimeout(timer)
+                clearInterval(interval)
+                return
+            }
+
+            position -= itemsCountVisible - 1
+            if (position < 0) {
+                const lastTime = position != 0
+                position = 0
+                clearTimeout(timer)
+                clearInterval(interval)
+                if (lastTime) {
+                    positionGrip()
+                    positionChanged(position)
+                }
+                return
+            }
+            positionGrip()
+            positionChanged(position)
+        }
+
+        function pageDown() {
+            if (grip.offsetTop + grip.offsetHeight > pageMousePosition) {
+                    clearTimeout(timer)
+                clearInterval(interval)
+                return
+            }
+
+            position += itemsCountVisible - 1
+            if (position > steps) {
+                const lastTime = position != 0
+                position = steps
+                clearTimeout(timer)
+                clearInterval(interval)
+                if (lastTime) {
+                    positionGrip()
+                    positionChanged(position)
+                }
+                return
+            }
+
+            positionGrip()
+            positionChanged(position)
+        }
+
+        function positionGrip() {
+            const top = 15 + position * step
+            grip.style.top = top + 'px'
+        }
+
+        var position = 0
+        /**
+         * {(position: number)=>void}
+         */
+        var positionChanged 
+        var gripTopDelta = -1
+        var gripping = false
+        /**
+         * {number}
+         */
+        var parentHeight
+        var offsetTop = 0
+        /**
+        * Ein zyklischer Timer
+        */
+        var timer
+        /**
+         * @type {number}
+         * Ein einmalige Timeout-Intervall
+         */
+        var interval
+        var pageMousePosition
+        /**
+         * @type {number}
+         */
+        var step
+        /**
+         * @type {number}
+         */
+        var steps
+        /**
+         * @type {number}
+         */
+        var itemsCountAbsolute
+        /**
+         * @type {number}
+         */
+        var itemsCountVisible
+    //    var tableView: TableView
+
+        return {
+            initialize: initialize,
+            itemsChanged: itemsChanged,
+            setPosition: setPosition
+        }    
+    }
+    return createScrollbar
+})()
+
+
