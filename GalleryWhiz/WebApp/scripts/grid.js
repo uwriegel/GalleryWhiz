e446f1a9576b210aa2429e66dfc8d88d2a4b5ee5
Uwe Riegel <uwriegel@gmail.com>
2018-01-29 16:56:29 +0100

    grid and vgrid

---
+++ GalleryWhiz/WebApp/scripts/grid.js
@@ -0,0 +1,73 @@
+﻿
+const createGrid = (function () {
+    /**
+     * Mechanismus zur variablen Aufteilung zweier Ansichten mit Hilfe eines Teilers
+     * @param {HTMLDivElement} gridContainer
+     * @param {HTMLElement} firstView die erste Ansicht
+     * @param {HTMLElement} secondView die zweite Ansicht
+     * @param {HTMLDivElement} gridSplitter der Teiler
+     * @param {any} onChanged Callback, wird aufgerufen wenn die Aufteilung geändert wurde
+     * @param {boolean} isVertical Die Aufteilung erfolgt vertikal
+     */
+    function createGrid(gridContainer,
+        firstView,
+        secondView,
+        gridSplitter,
+        onChanged,
+        isVertical) {
+        gridSplitter.addEventListener('mousedown', evt => {
+            if (evt.which != 1)
+                return
+            let firstPercent
+            const initialPosition = isVertical ? evt.pageY : evt.pageX
+            const start = isVertical ? firstView.offsetHeight : firstView.offsetWidth
+
+            const onmousemove = (evt) => {
+                const delta = (isVertical ? evt.pageY : evt.pageX) - initialPosition
+
+                const position = start + delta
+                firstPercent = position / ((isVertical ? gridContainer.offsetHeight : gridContainer.offsetWidth) - 6) * 100
+                if (firstPercent < 4)
+                    firstPercent = 4
+                else if (firstPercent > 96)
+                    firstPercent = 96
+                setSize(firstPercent)
+
+                evt.stopPropagation()
+                evt.preventDefault()
+            }
+
+            const onmouseup = (evt) => {
+                window.removeEventListener('mousemove', onmousemove, true)
+                window.removeEventListener('mouseup', onmouseup, true)
+                evt.stopPropagation();
+                evt.preventDefault();
+                if (onChanged)
+                    onChanged(firstPercent)
+            }
+
+            window.addEventListener('mousemove', onmousemove, true)
+            window.addEventListener('mouseup', onmouseup, true)
+
+            evt.stopPropagation()
+            evt.preventDefault()
+        }, true)
+
+        function setSize(firstPercent) {
+            const secondPercent = 100 - firstPercent;
+            gridSplitter.style[isVertical ? 'top' : 'left'] = `calc(${firstPercent}% - 3px)`
+            firstView.style[isVertical ? 'height' : 'width'] = `calc(${firstPercent}% - 3px)`
+            secondView.style[isVertical ? 'top' : 'left'] = `calc(${firstPercent}% + 3px)`
+            secondView.style[isVertical ? 'height' : 'width'] = `calc(${secondPercent}% - 3px)`
+        }
+
+        return {
+            /**
+             * Setzen der Größen der Elemente
+             * @param firstPercent Prozentzahl der Größe des ersten Elements
+             */
+            setSize: setSize
+        }
+    }
+    return createGrid
+})()
---
+++ GalleryWhiz/WebApp/scripts/vgrid.js
@@ -0,0 +1,50 @@
+﻿
+const createVerticalGrid = (function () {
+    /**
+     * Oben beide CommanderViews, unten, wenn eingeschaltet, der Viewer
+     * @param {HTMLDivElement} gridContainer der beinhaltende Container
+     * @param {HTMLElement} topView die obere Ansicht
+     * @param {HTMLElement} bottomView die untere Ansicht
+     * @param {HTMLDivElement} gridSplitter der Teiler
+     * @param {() => void} onChanged Callback, wird aufgerufen wenn die Aufteilung geändert wurde
+     */
+    function createVerticalGrid(
+        gridContainer,
+        topView,
+        bottomView,
+        gridSplitter,
+        onChanged) {
+
+        let topPercent = localStorage["vgrid"]
+        if (!topPercent)
+            topPercent = 70
+
+        const grid = createGrid(gridContainer, topView, bottomView, gridSplitter,
+            (firstPercent) => {
+                topPercent = firstPercent
+                localStorage["vgrid"] = firstPercent
+                onChanged()
+            },
+            true)
+
+        grid.setSize(topPercent)
+        switchBottom(false)
+
+        /**
+         * Ein/Ausblenden der unteren Ansicht
+         * @param {boolean} preview
+         */
+        function switchBottom(preview) {
+            if (preview) {
+                bottomView.classList.remove("hidden")
+                gridSplitter.classList.remove("hidden")
+                topView.style.height = `calc(${topPercent}% - 3px)`
+            } else {
+                bottomView.classList.add("hidden")
+                gridSplitter.classList.add("hidden")
+                topView.style.height = "100%"
+            }
+        }
+    }
+    return createVerticalGrid
+})()
